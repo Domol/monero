60355467eca834d45aacb52d7b8cdb4cdcec109f
Dominik Olczyk <dominik@cryptosphere-systems.com>
2019-04-26 15:01:51 +0200

    Fix dev scripts

---
+++ dev_scripts/docker_build.sh
@@ -0,0 +1,1 @@
+docker run --rm --volume '/Users/dominik/Projects/cryptosphere/monero/:/monero/' -it xmrtopatch bash -c 'chmod +x /monero/dev_scripts/build_wallet_cli.sh && /monero/dev_scripts/build_wallet_cli.sh'
